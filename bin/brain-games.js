dae0316f86b731da7d5fe6ea4078ea70f5aec1a7
Shuhrat <shuh47@yandex.ru>
2020-10-28 20:16:27 +0300

    Step 4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 // такая запись позволит выполнить код, который в нём расположен
-import { default as hello } from '../src/cli.js';
+import hello from '../src/cli';
 
 console.log('Welcome to the Brain Games!');
 hello();
