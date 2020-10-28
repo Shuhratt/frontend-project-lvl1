e8ce678ca5c8ffed5ea7b315df0cd13c1341f72d
Shuhrat <shuh47@yandex.ru>
2020-10-28 13:39:11 +0300

    Step 3

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import promptly from 'promptly';
 
-export default async() => {
-    const name = await promptly.prompt('Your answer: ');
-    console.log(name)
+export default async () => {
+    const name = await promptly.prompt('May I have your name?');
+    console.log(`Hello, ${name}!`)
 };
