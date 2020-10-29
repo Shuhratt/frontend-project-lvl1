5f96ca1ff5946eaa782e3fc4f6096d0636251911
Shuhrat <shuh47@yandex.ru>
2020-10-29 13:54:44 +0300

    Step 4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 // такая запись позволит выполнить код, который в нём расположен
-import hello from '../src/cli';
+import hello from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 hello();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import promptly from 'promptly';
 
-export default async () => { // ? await
+export default async () => { // ? async
   const name = await promptly.prompt('May I have your name?'); // ? await
   console.log(`Hello, ${name}!`);
 };
