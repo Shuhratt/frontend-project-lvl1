05d1a2b8ca621a70bda83675ccc32249337aafd6
Shuhrat <shuh47@yandex.ru>
2020-10-30 13:04:10 +0300

    Step 5

--- src/cli.js
+++ src/cli.js
@@ -4,5 +4,3 @@ export default async () => { // ? async
   const name = await promptly.prompt('May I have your name?'); // ? await
   console.log(`Hello, ${name}!`);
 };
-
-
--- src/even.js
+++ src/even.js
@@ -5,13 +5,15 @@ export default async () => {
   console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  let randomCount = (Math.random() * 10).toFixed(0)
-  console.log(`Question: ${randomCount}`)
-  const answer = await promptly.prompt(`Answer:`);
+  const randomCount = (Math.random() * 10).toFixed(0);
+  const isEven = (count) => (count % 2 === 0);
 
+  console.log(`Question: ${randomCount}`);
+  let answer = await promptly.prompt('Answer:');
 
-
-
-
-
+  if (isEven(randomCount)) {
+    console.log(answer == 'yes')
+  } else{
+    console.log(answer == 'no')
+  }
 };
