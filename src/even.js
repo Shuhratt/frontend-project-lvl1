7a3841a5d91fb9f1c85bb483c51e055a404fae57
Shuhrat <shuh47@yandex.ru>
2020-10-30 12:49:31 +0300

    Step 5

--- src/even.js
+++ src/even.js
@@ -3,7 +3,15 @@ import promptly from 'promptly';
 export default async () => {
   const name = await promptly.prompt('May I have your name?');
   console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".')
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  const work = await promptly.prompt('Че ка делал');
+  let randomCount = (Math.random() * 10).toFixed(0)
+  console.log(`Question: ${randomCount}`)
+  const answer = await promptly.prompt(`Answer:`);
+
+
+
+
+
+
 };
