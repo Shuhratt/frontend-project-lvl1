ee4a4e75f5e67c2592b53528fd0f7c5779fcbb5e
Shuhrat <shuh47@yandex.ru>
2020-10-31 12:02:58 +0300

    Step 5

--- src/even.js
+++ src/even.js
@@ -1,21 +1,42 @@
 import promptly from 'promptly';
 
+const validator =  (value) => {
+  if(value !== 'yes'){
+    throw new Error('"yes" is wrong answer ;(. Correct answer was "no".'); // "да" - это неправильный ответ; (правильный ответ был "нет"
+  } else{
+    console.log(`Correct!`);
+  }
+};
+
 export default async () => {
   const name = await promptly.prompt('May I have your name?');
+  let userAnswers = []
   console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  const randomCount = (Math.random() * 10).toFixed(0);
+  const randomCount = (Math.random() * 10).toFixed(0) || 1;
   const isEven = (count) => (count % 2 === 0);
 
-  console.log(`Question: ${randomCount}`);
-  const answer = await promptly.confirm('Answer:');
+    try {
+      if(userAnswers.length === 3){
 
-  if (isEven(randomCount)) {
-    console.log('Answer:', answer === true ? answer : '');
-  } else {
-    console.log('Answer:', answer === false ? answer : '');
-  }
+      }
+      console.log(`Question: ${randomCount}`);
+
+      if (isEven(randomCount)) {
+        const answerEvenTrue = await promptly.prompt('Your answer: ', { validator, retry: false });
+      } else {
+
+      }
+
+
+    } catch (err) {
+      console.error(`${err.message}`);
+      console.error(`Let\'s try again, ${name}!`)
+    }
+
+
+
 
   // npm list -g --depth=0
 };
