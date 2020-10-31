204c7129e8960e26dc7683e6b6add0284fdae335
Shuhrat <shuh47@yandex.ru>
2020-10-31 17:16:09 +0300

    Step 5

--- src/even.js
+++ src/even.js
@@ -1,45 +1,40 @@
 import promptly from 'promptly';
 
-const isEven = (count) => count % 2 === 0 ? 'yes' : 'no';
+const isEven = (count) => (count % 2 === 0 ? 'yes' : 'no');
 
 const messageError = (answer, answerRight) => {
-  throw new Error(`"${answer}" is wrong answer ;(. Correct answer was "${answerRight}".`) // "да" - это неправильный ответ ;(. правильным ответом было "нет".
-}
-
+  throw new Error(`"${answer}" is wrong answer ;(. Correct answer was "${answerRight}".`); // "да" - это неправильный ответ ;(. правильным ответом было "нет".
+};
 
 export default async () => {
   const name = await promptly.prompt('May I have your name?');
   console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-    try {
-      let i = 1
-      while (i <= 3) {
-        let randomCount = (Math.random() * 10).toFixed(0);
-        console.log(`Question: ${randomCount}`);
-        const answer = await promptly.prompt('Your answer: ', { retry: false });
+  try {
+    let i = 1;
+    while (i <= 3) {
+      const randomCount = (Math.random() * 10).toFixed(0);
+      console.log(`Question: ${randomCount}`);
+      const answer = await promptly.prompt('Your answer: ', { retry: false });
 
-        if(isEven(randomCount) === 'yes'){
-          if(answer == 'yes'){
-            console.log(`Correct!`);
-          } else {
-            messageError(answer, isEven(randomCount))
-          }
-        } else{
-          if(answer == 'no'){
-            console.log(`Correct!`);
-          } else {
-            messageError(answer, isEven(randomCount))
-          }
+      if (isEven(randomCount) === 'yes') {
+        if (answer === 'yes') {
+          console.log('Correct!');
+        } else {
+          messageError(answer, isEven(randomCount));
         }
-
-        i++
+      } else if (answer === 'no') {
+        console.log('Correct!');
+      } else {
+        messageError(answer, isEven(randomCount));
       }
-      await console.log(`Congratulations, ${name}!`)
 
-    } catch (err) {
-      console.error(`${err.message}`);
-      console.error(`Let\'s try again, ${name}!`)
+      i++;
     }
-
+    await console.log(`Congratulations, ${name}!`);
+  } catch (err) {
+    console.error(`${err.message}`);
+    console.error(`Let's try again, ${name}!`);
+  }
 };
