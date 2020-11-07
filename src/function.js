64aa6cbc0bc8de4043288330e5e37673d6f30ac3
Shuhrat <shuh47@yandex.ru>
2020-11-07 16:15:58 +0300

    Check

--- src/function.js
+++ src/function.js
@@ -1,5 +1,5 @@
 // Сообщение об ошибке
-export const messageError = (answerUser, answerRight) => {
+export const messageErrorShow = (answerUser, answerRight) => {
   throw new Error(`"${answerUser}" is wrong answer ;(. Correct answer was "${answerRight}".`); // "nn" - это неправильный ответ ;(. правильным ответом было "nn".
 };
 
@@ -8,7 +8,7 @@ export const checking = (answerUser, answerRight) => {
   if (answerUser === answerRight) {
     console.log('Correct!');
   } else {
-    messageError(answerUser, answerRight);
+    messageErrorShow(answerUser, answerRight);
   }
 };
 
@@ -16,10 +16,10 @@ export const checking = (answerUser, answerRight) => {
 export const randomCount = (count) => Math.floor(Math.random() * count);
 
 // Четное ли  число для brain-games
-export const isEven = (count) => (count % 2 === 0 ? 'yes' : 'no');
+export const isEven = (count) => (count % 2 === 0 ? 'yes' : 'no'); // hasEven ?
 
 // Натуральное ли  число для brain-prime
-export const isPrime = (count) => {
+export const isPrime = (count) => { // isPrime ?
   let result = true;
   for (let i = count - 1; i <= count && i > 1; i -= 1) {
     if (count % i === 0) {
