db04a545da110783c47cecba2747e2bab6ab78fb
Shuhrat <shuh47@yandex.ru>
2020-11-17 20:05:52 +0300

    Edit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import promptly from 'promptly';
 
 // Ядро игры
-export default async (textRules, conditions) => {
+export default async (textRules, playGames) => {
   console.log('Welcome to the Brain Games!');
   const name = await promptly.prompt('May I have your name?');
   console.log(`Hello, ${name}!`);
@@ -10,9 +10,9 @@ export default async (textRules, conditions) => {
   const countSteps = 3;
   try {
     for (let i = 1; i <= countSteps; i += 1) {
-      await conditions();
+      await playGames();
     }
-    await console.log(`Congratulations, ${name}!`);
+    console.log(`Congratulations, ${name}!`);
   } catch (err) {
     console.error(`${err.message}`);
     console.error(`Let's try again, ${name}!`);
